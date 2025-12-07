4b80c273d38995959c0e3248ddf55010697e2630
fdemb <69842744+fdemb@users.noreply.github.com>
2025-12-07 01:27:01 +0100

    feat: enable sqlite wal mode

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ import * as schema from "./schema";
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set.");
 
 const client = new Database(process.env.DATABASE_URL);
+client.run("PRAGMA journal_mode = WAL;");
 
 const db = drizzle({
   client,
